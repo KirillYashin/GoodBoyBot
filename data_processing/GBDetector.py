41c83a27537337f37ff2459b00dfa2376f962413
Sergey Oparin <oparin.sergey.nng@gmail.com>
2021-07-16 17:28:59 +0300

    Added models cascade (detector > classifier > result)

--- data_processing/GBDetector.py
+++ data_processing/GBDetector.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 from openvino.inference_engine import IECore
@@ -6,54 +5,16 @@ import sys
 import logging as log
 import argparse
 import pathlib
-import random
-import colorsys
+from time import time
+
+from GBClassifier import list_classifier
+
 #from performance_metrics import PerformanceMetrics
 
 sys.path.append('C:\\Program Files (x86)\\Intel\\openvino_2021\\deployment_tools\\open_model_zoo\\demos\\common\\python')
 import models
 from pipelines import AsyncPipeline
 from images_capture import open_images_capture
-
-class ColorPalette:
-    def __init__(self, n, rng=None):
-        assert n > 0
-
-        if rng is None:
-            rng = random.Random(0xACE)
-
-        candidates_num = 100
-        hsv_colors = [(1.0, 1.0, 1.0)]
-        for _ in range(1, n):
-            colors_candidates = [(rng.random(), rng.uniform(0.8, 1.0), rng.uniform(0.5, 1.0))
-                                 for _ in range(candidates_num)]
-            min_distances = [self.min_distance(hsv_colors, c) for c in colors_candidates]
-            arg_max = np.argmax(min_distances)
-            hsv_colors.append(colors_candidates[arg_max])
-
-        self.palette = [self.hsv2rgb(*hsv) for hsv in hsv_colors]
-
-    @staticmethod
-    def dist(c1, c2):
-        dh = min(abs(c1[0] - c2[0]), 1 - abs(c1[0] - c2[0])) * 2
-        ds = abs(c1[1] - c2[1])
-        dv = abs(c1[2] - c2[2])
-        return dh * dh + ds * ds + dv * dv
-
-    @classmethod
-    def min_distance(cls, colors_set, color_candidate):
-        distances = [cls.dist(o, color_candidate) for o in colors_set]
-        return np.min(distances)
-
-    @staticmethod
-    def hsv2rgb(h, s, v):
-        return tuple(round(c * 255) for c in colorsys.hsv_to_rgb(h, s, v))
-
-    def __getitem__(self, n):
-        return self.palette[n % len(self.palette)]
-
-    def __len__(self):
-        return len(self.palette)
 
 
 def get_plugin_configs(device, num_streams, num_threads):
@@ -97,12 +58,53 @@ def build_argparser():
     parser.add_argument('-t', '--prob_threshold', default=0.5, type=float,
         help='Optional. Probability threshold for detections filtering.')
     return parser
-  
-  
-def crop_pets(frame, detections, threshold):
+
+def pet_detection(out, frame, threshold):
+    (height, width) = frame.shape[:2]
+    cats = []
+    dogs = []
+    for detection in out.reshape(-1, 7):
+        det_class = int(detection[1])
+        confidence = float(detection[2])
+        if det_class == 15 or det_class == 16:
+            if confidence > threshold:
+                xmin = max(int(detection[3] * width), 0)
+                ymin = max(int(detection[4] * height), 0)
+                xmax = min(int(detection[5] * width), width)
+                ymax = min(int(detection[6] * height), height)
+                if det_class == 16:
+                    dogs.append(frame[ymin:ymax, xmin:xmax])
+                    log.info("New dog has deteced")
+                else:
+                    cats.append(frame[ymin:ymax, xmin:xmax])
+                    log.info("New cat has deteced")
+    
+    i = 0
+    for dog in dogs:
+        cv2.imshow('Dog #{}'.format(i), dog)
+        i+=1
+    i=0
+    for cat in cats:
+        cv2.imshow('Cat #{}'.format(i), cat)
+        i+=1  
+        
+    return dogs, cats
+
+
+def pytorch_detection(frame, netsize, net, thr):
+    # Prepare input blob and perform an inference.
+    blob = cv2.dnn.blobFromImage(frame, 1.0, netsize, ddepth = cv2.CV_8U)
+    net.setInput(blob)
+    out = net.forward()
+
+    return pet_detection(out, frame, thr)
+ 
+def yolo_detection(frame, detections, threshold):
     size = frame.shape[:2]
     cats = []
     dogs = []
+    dogs_count = 0
+    cats_count = 0
     for detection in detections:
         det_class = int(detection.id)
         # If score more than threshold and detection is a pet
@@ -113,24 +115,31 @@ def crop_pets(frame, detections, threshold):
                 xmax = min(int(detection.xmax), size[1])
                 ymax = min(int(detection.ymax), size[0])
                 if det_class == 16:
+                    dogs_count += 1
                     dogs.append(frame[ymin:ymax, xmin:xmax])
                     log.info("New dog has deteced")
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                    cv2.rectangle(frame, (xmin, ymin-30), (xmin+80, ymin), (0, 255, 0), -1)
+                    cv2.putText(frame, ' Dog #{}'.format(dogs_count),(xmin, ymin - 7), 
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 0), 2) 
                 else:
+                    cats_count += 1
                     cats.append(frame[ymin:ymax, xmin:xmax])
                     log.info("New cat has deteced")
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (xmin, ymin-30), (xmin+80, ymin), (0, 0, 255), -1)
+                    cv2.putText(frame, ' Cat #{}'.format(cats_count),(xmin, ymin - 7), 
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 0), 2) 
 
-    i = 0
+    '''i = 0
     for dog in dogs:
         cv2.imshow('Dog #{}'.format(i), dog)
         i+=1
     i=0
     for cat in cats:
         cv2.imshow('Cat #{}'.format(i), cat)
-        i+=1
+        i+=1'''
     
-    '''cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 255), 2)
-    cv2.putText(frame, '{} {:.1%}'.format(det_label, detection.score),
-                (xmin, ymin - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)'''    
         
     return dogs, cats
 
@@ -140,38 +149,44 @@ def main():
                     level=log.INFO, stream=sys.stdout)
     args = build_argparser().parse_args()
     log.info("Start OpenVINO object detection")
-
+    start = time()
     # Initialize data input
     cap = open_images_capture(args.input, True)
-    # Initialize OpenVINO
-    ie = IECore()
-    # Initialize Plugin configs
-    plugin_configs = get_plugin_configs('CPU', 0, 0)
-    # Load YOLOv3 model
-    detector = models.YOLO(ie, pathlib.Path(args.model), None, 
-                            threshold=args.prob_threshold, keep_aspect_ratio=True)
-    
-
-    # Initialize async pipeline
-    #metrics = PerformanceMetrics()
-    detector_pipeline = AsyncPipeline(ie, detector, plugin_configs, device='CPU', 
-                                        max_num_requests=1)
     Dogs = []
     Cats = []
-    # Get one image 
-    img = cap.read()
-    # Start processing frame asynchronously
-    frame_id = 0 
-    detector_pipeline.submit_data(img,frame_id,{'frame':img,'start_time':0})
-    detector_pipeline.await_any()
-    # Get detection result
-    results, meta = detector_pipeline.get_result(frame_id)
+    if 'yolo' in args.model:
+        # Initialize OpenVINO
+        ie = IECore()
+        # Initialize Plugin configs
+        plugin_configs = get_plugin_configs('CPU', 0, 0)
+        # Load YOLOv3 model
+        detector = models.YOLO(ie, pathlib.Path(args.model), None, 
+                                threshold=args.prob_threshold, keep_aspect_ratio=True)
+        
+        # Initialize async pipeline
+        detector_pipeline = AsyncPipeline(ie, detector, plugin_configs, device='CPU', 
+                                            max_num_requests=1)
+
+        # Get one image 
+        img = cap.read()
+        # Start processing frame asynchronously
+        frame_id = 0 
+        detector_pipeline.submit_data(img,frame_id,{'frame':img,'start_time':0})
+        detector_pipeline.await_any()
+        # Get detection result
+        results, meta = detector_pipeline.get_result(frame_id)
 
     # Get list of detections in the image
     if results:
+        Dogs, Cats = yolo_detection(img, results, args.prob_threshold)
         cv2.imshow('Original', img)
-        Dogs, Cats = crop_pets(img, results, args.prob_threshold)
+        classification_start = time()
+        result = list_classifier(Dogs)
+    end = time()
 
+    # Time usage
+    log.info("Classification time: {} sec".format(int((end - classification_start)*100)/100))
+    log.info("Usage time: {} sec".format(int((end - start)*100)/100))
     cv2.waitKey(0)
       
     # Destroy all windows
