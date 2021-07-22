118145ad8aa0d94f903c88c9e6f4f8e31f75d6b2
Vladislav-Kanev <vokanev@edu.hse.ru>
2021-07-22 12:13:27 +0300

    bot with buttons version

--- tg_GBBot/TG_launcher.py
+++ tg_GBBot/TG_launcher.py
@@ -10,6 +10,7 @@ import numpy as np
 from PIL import Image
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from GBDetector import Detector, get_breed_info, translator
 # bot init
 bot = Bot(token=config.TOKEN)
@@ -38,49 +39,53 @@ async def photo_reaction(message):
     open_cv_image = np.array(img)
 
     open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2RGB)
-    Dogs = []
-    Cats = []
-    Conf = []
-    Dogs, Cats, Conf, out = Detector(open_cv_image)
+    dogs = []
+    cats = []
+    conf = []
+    dogs, cats, conf, out = Detector(open_cv_image)
     out = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2RGB)
-    if len(Dogs) == 0:
-        if len(Cats) == 0:
+    if len(dogs) == 0:
+        if len(cats) == 0:
             await message.answer('Я никого не нашел на фото(')
         else:
             await message.answer('Кис кис, кис кис. Я ботик, ты котик)')
-    elif len(Dogs) == 1:
+    elif len(dogs) == 1:
         output_image = Image.fromarray(out)
         stream_image = BytesIO()
         stream_image.name = "Dogs.jpg"
         output_image.save(stream_image, 'JPEG')
         stream_image.seek(0)
-        breed = translator(Dogs[0][0])
+        breed = translator(dogs[0][0])
         breed_info, image_url = get_breed_info(breed)
-        breed_info = "Мне кажется, что это ", breed + '\n' + breed_info
+        breed_info = f"Мне кажется, что это {breed}. Я уверен в этом на процентов {int(conf[0] * 10000) / 100}%.\n" \
+                     f"{breed_info}"
         await message.answer_photo(image_url, caption=breed_info)
-        #await message.answer_photo(stream_image, caption=breed)
-        
+
     else:
         output_image = Image.fromarray(out)
         stream_image = BytesIO()
         stream_image.name = "Dogs.jpg"
         output_image.save(stream_image, 'JPEG')
         stream_image.seek(0)
-        # await was here
-        await message.answer_photo(stream_image, caption='Выбери породу какого песика ты хочешь узнать)')
-        await breed_answering(message, Dogs)
-    @dp.message_handler()
-    async def test(message1):
-        print(message1.text)
+        button_list = await breed_answering(message, dogs)
+        await message.answer_photo(stream_image, caption='Выбери породу какого песика ты хочешь узнать)',
+                                   reply_markup=button_list)
+    return
 
-def breed_answering(message: types.Message, Dogs):
-    for breed in Dogs:
-        await message.answer("Что-то не припомню такой породы") 
+
+async def breed_answering(message: types.Message, dog_list):
+    dog_set = {i[0] for i in dog_list}
+    button_list = ReplyKeyboardMarkup(resize_keyboard=True)
+    for breed in dog_set:
+        button = KeyboardButton(translator(breed))
+        button_list.add(button)
+    return button_list
+
 
 # dog breed answer
-#@dp.message_handler()
+@dp.message_handler()
 async def echo(message: types.Message):
-    breed = message.text.lower().title()
+    breed = message.text.lower().capitalize()
     breed_info, image_url = get_breed_info(breed)
     if breed_info == " ":
         await message.answer("Что-то не припомню такой породы")
